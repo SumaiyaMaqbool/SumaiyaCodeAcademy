a2e0a213de1f1449773fbc2c5163c95d3bb8634a
SumaiyaMaqbool <smymq99@gmail.com>
2023-05-09 16:13:17 +0400

    SumaiyaCodeAcademy

--- Day2ProblemAnswerFindTheLargestNumber.py.py
+++ Day2ProblemAnswerFindTheLargestNumber.py.py
@@ -16,13 +16,17 @@ elif(z>x and z>y):
 num1 = int(input("Enter first number: "))
 num2 = int(input("Enter second number: "))
 num3 = int(input("Enter third number: "))
+largest = 0
 if(num1==num2==num3):
     print ("The entered numbers are equal")
 elif (num1 >= num2) and (num1 >= num3):
     largest = num1
+    print("The largest number between", num1, ",", num2, "and", num3, "is", largest)
+
 elif (num2 >= num1) and (num2 >= num3):
     largest = num2
+    print("The largest number between", num1, ",", num2, "and", num3, "is", largest)
+
 else:
     largest = num3
-
-print("The largest number between", num1, ",", num2, "and", num3, "is", largest)
+    print("The largest number between", num1, ",", num2, "and", num3, "is", largest)
