1929dcaf8e129dd7c1bc1d161621bc4fff8a2429
SumaiyaMaqbool <smymq99@gmail.com>
2023-05-09 13:50:36 +0400

    SumaiyaCodeAcademy

--- Day2ProblemAnswer.py.py
+++ Day2ProblemAnswer.py.py
@@ -1,14 +1,28 @@
-x= input("Enter the first number: ")
+'''x= input("Enter the first number: ")
 x= int(x)
 y= input("Enter Second Number : ")
 y= int(y)
 z= input("Enter Third Number : ")
 z= int(z)
-if (x>y and x>z):
+if(x==y==z):
+        print ("The entered numbers are equal")
+elif (x>y and x>z):
     print(x, " is the largest number")
 elif(y>x and y>z):
         print(y, " is the largest number")
 elif(z>x and z>y):
-        print(z, " is the largest number")
-elif(x==y==z):
-        print ("The entered numbers are equal")
+        print(z, " is the largest number")'''
+
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+num3 = int(input("Enter third number: "))
+if(num1==num2==num3):
+    print ("The entered numbers are equal")
+elif (num1 >= num2) and (num1 >= num3):
+    largest = num1
+elif (num2 >= num1) and (num2 >= num3):
+    largest = num2
+else:
+    largest = num3
+
+print("The largest number between", num1, ",", num2, "and", num3, "is", largest)
